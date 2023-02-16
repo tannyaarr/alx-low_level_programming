b9ad2510f36393f8b2b8499a63165a6b283702b4
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-02-16 10:04:07 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts 
- *
- * return: 0
+/**
+ * main - Entry Point
+ *Return: Always 0 (Success)
 */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
+
 }	
