c880148652ca637301d1acebbac9777a941c5fb1
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-02-16 11:58:29 -0800

    Printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <shdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf.c("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+
+}
