bca82efbaa31815c38e8b6bd7b9fe7fd016a9355
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-02-18 01:09:14 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time.h>
+
+/**
+ * main - all alphabets in lowercase except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lc, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+	if (lc != e && lc != q)
+	putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
+
