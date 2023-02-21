5f6e6bf8ac45c83c1d532380e98dff5747ec0f33
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-02-21 04:36:41 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lowercase followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
