63eaecab849d6c08040fd0330ab539a1636dbfc8
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-02-21 15:31:33 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * time_table - Prints out the 9 times table starting with 0
+ * times_table - Prints out the 9 times table starting with 0
  * Return: empty output
  *
  */
