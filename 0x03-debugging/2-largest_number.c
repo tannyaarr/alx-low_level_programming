04092c112afec277df9df2adbd6d7a8da01b34bf
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-02-23 11:51:57 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,18 +9,16 @@
  */
 
 int largest_number(int a, int b, int c)
+
 {
+
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 	largest = a;
 	}
-	else if (a > b && c > a)
-	{
-	largest = c;
-	}
-	else if (b > c)
+	else if (b >= a && b >= c)
 	{
 	largest = b;
 	}
@@ -28,6 +26,5 @@ int largest_number(int a, int b, int c)
 	{
 	largest c;
 	}
-
 	return (largest);
 }
