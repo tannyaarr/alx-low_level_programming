2a28f81eb424864f4379c06d4e790c73bb67f7bd
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-03-12 13:44:06 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints its name
+ * @agrc: argument count
+ * @agrv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
