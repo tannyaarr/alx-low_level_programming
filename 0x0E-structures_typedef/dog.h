893c0819a1298d8b924369e2c66c8f0253c85820
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-03-20 14:40:19 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's information
+ * @name: first input
+ * @age: second input
+ * @owner: third input
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
