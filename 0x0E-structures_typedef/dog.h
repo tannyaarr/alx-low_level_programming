14c8a1b5fde5df830ab7cb9fe1975a079cac3005
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-03-20 15:57:50 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,11 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+ * dog_t- typedef for struct dog
+ */
+typedef struct dog dog_t;
+void free_dog(dog_t *d);
 #endif /* DOG_H */
