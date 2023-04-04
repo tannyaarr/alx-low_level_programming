97eb76baf014fdec5b0100489c74323454bd6b0e
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-04-04 16:45:09 -0700

    delete head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,13 +6,14 @@
  * @head: pointer to the first element in the linked list
  *
  * Return: the heads node's data (n)
+ * or 0 if the linked list is empty
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 	int x;
 
-	if (head || !*head)
+	if (!head || !*head)
 		return (0);
 
 	x = (*head)->n;
