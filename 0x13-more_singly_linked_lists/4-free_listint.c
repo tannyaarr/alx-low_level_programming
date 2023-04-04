a4546001175ac5185f4718f093285fdf91c304b5
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-04-04 16:02:45 -0700

    frees list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: listint_t list to be freed
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
