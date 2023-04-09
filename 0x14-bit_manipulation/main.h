18de2cbb6d1ea9347d37410f3bb9d67255f61f71
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-04-09 14:00:28 -0700

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,4 +1,4 @@
-#ifndef MIAN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 unsigned int binary_to_uint(const char *b);
@@ -8,6 +8,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
-
 
 #endif
