3711526ba4f636bd09fbffaebb508bf6a6235938
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-04-12 12:20:36 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd;
+	int w;
+	int len;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,11 +23,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (len = 0; text_content[len]; len++)
+			;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
