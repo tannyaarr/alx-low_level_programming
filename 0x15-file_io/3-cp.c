cfc81a6cfa7f1a0bb5a5d38e5608d5acbbc4b5f7
Tanyaradzwa Chiramba <tanyachiras@gmail.com>
2023-05-09 06:23:19 -0700

    updated copy content to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
  * create_buffer - allocate 1024 bytes for a buffer
@@ -50,7 +52,7 @@ void close_file(int fd)
  * Description: if the argument count is incorrect - exit code 97.
  * if file_from does not exist or cannot be read - exit code 98.
  * if file_to cannot be created or written to - exit code 99
- * if file_to or file_from cannot be closed exit code 100
+ * if file_to or file_from cannot be closed - exit code 100
  */
 
 int main(int argc, char *argv[])
