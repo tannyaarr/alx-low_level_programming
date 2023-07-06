36ee5fcf77b3373adadf0efd24bc6995df46d7a6
tannyaarr <tanyachiras@gmail.com>
2023-07-06 07:10:19 -0700

    binary to uint

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,8 +11,8 @@ void print_binary(unsigned long int n)
 {
 	int i, count = 0;
 	unsigned long int current;
-	
-	for (i = 63; i >=0; i--)
+
+	for (i = 63; i >= 0; i--)
 	{
 		current = n >> i;
 		if (current & 1)
