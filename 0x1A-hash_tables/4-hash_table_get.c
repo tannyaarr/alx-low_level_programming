0945d9601311d95a5cc63439a507eee438535d89
tannyyaarr <tanyachiras@gmail.com>
2023-11-07 05:44:00 -0800

    cleared betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,7 +15,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *current;
 	unsigned long int index = key_index
-		((unsigned char *)key,ht->size);
+		((unsigned char *)key, ht->size);
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
